d2954c7e6e934f9409580145f0cb7b0d3ad3a557
Suman Baral <115060661+suman6517@users.noreply.github.com>
2024-03-19 05:46:32 -0400

    Time: 118 ms (86.88%), Space: 42.5 MB (41.25%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        if (s.length() <= 2) 
+        {
+            return s;
+        }
+
+        string ans = "";
+
+        for (int i = 0; i < s.length() - 2; i++) {
+
+        if((s[i] == s[i+1]) && (s[i] == s[i+2]))
+        {
+            continue;
+        }
+        ans += s[i];
+        }
+
+        ans += s[s.length()-2];
+        ans += s[s.length()-1];
+
+        return ans;
+
+
+    }
+};
