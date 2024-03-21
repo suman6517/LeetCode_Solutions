4e8c43b8f492181890c2a2eac51227f5d64ad892
Suman Baral <115060661+suman6517@users.noreply.github.com>
2024-03-21 10:34:43 -0400

    Time: 3 ms (89.3%), Space: 16.1 MB (49.5%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,73 +1,67 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target)
+    vector<int> searchRange(vector<int>& nums, int target) 
     {
-        // vector<int>ans ;
-        // int firstOccurance = firstPosition(nums,  target);
-        // ans.push_back(firstOccurance);
-        // int lastOccurance = lastPosition(nums,target);
-        // ans.push_back(lastOccurance);
-        // return ans;
-        return { firstPosition(nums,  target) , lastPosition(nums,target)};
-
-        
+         return { firstPosition(nums,  target) , LastPosition(nums,target)};
     }
-    int firstPosition(vector<int>& nums, int target)
+
+
+    int firstPosition(vector<int>& nums, int target) 
     {
         int start = 0;
-        int end = nums.size()-1;
-        int ans =-1;
-
-        while(start <=end)
-        {
-            int mid =start+(end-start)/2;
-            if(nums[mid] == target)
-            {
-                ans = mid;
-                end = mid -1;
-            }
-            else if (nums[mid] <target)
-            {
-                start = mid+1;
-            }
-            else
-            {
-                end = mid -1;
-            }
-
-           // mid = start+ (end-start) /2;
-
-        }
-        return ans ;
-    }
-
-
-    int lastPosition(vector<int>& nums, int target)
-    {
-        int start =0;
-        int end = nums.size()-1;
-       // int mid = start+ (end -start)/2;
         int ans = -1;
-        while (start <= end )
+        int end = nums.size() - 1;
+        while (start <= end) 
         {
-            int mid =start+(end-start)/2;
-            if (nums[mid] == target)
-            {
-                ans = mid ;
-                start = mid +1;
+            int mid = start + (end - start)/2;
 
-            }
-            else if (nums [mid] <target)
+            if (nums[mid] == target)
+             {
+                 ans = mid;
+                end = mid - 1;
+
+            } 
+           else if (nums[mid] < target) 
             {
-               start = mid+1;
-            
-            }
-            else
+                start = mid +1;
+            } 
+            else 
             {
-                end = mid -1;
+                end = mid - 1;
             }
-           // mid =  start + (end - start )/2;
         }
+
         return ans;
     }
-};
+
+    int LastPosition(vector<int>& nums, int target) 
+    {
+        int start = 0;
+        int ans = -1;
+        int end = nums.size() - 1;
+        while (start <= end) 
+        {
+            int mid = start + (end - start)/2;
+
+            if (nums[mid] == target) 
+            {
+                 ans = mid;
+                start = mid + 1;
+
+            } 
+            else if (nums[mid] < target) 
+            {
+                start = mid +1;
+            } 
+            else 
+            {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+
+    }
+
+        
+    };
