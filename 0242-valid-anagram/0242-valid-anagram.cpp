78b6bb4510671695b0b79a04226f52e4bbee8ba3
Suman Baral <115060661+suman6517@users.noreply.github.com>
2024-04-12 12:05:05 +0530

    Time: 17 ms (6.42%), Space: 10.5 MB (6.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -9,19 +9,15 @@ public:
         map<char ,int>m1;
         map<char,int>m2;
 
-        // for(auto it1 : s)
-        // {
-        //     m1[it1]++;
+        for(auto it1 : s)
+        {
+            m1[it1]++;
 
-        // }
-        // for(auto it2 : s)
-        // {
-        //     m2[it2]++;
+        }
+        for(auto it2 : t)
+        {
+            m2[it2]++;
 
-        // }
-         for(int i = 0; i < s.size(); i++){
-            m1[s[i]]++;
-            m2[t[i]]++;
         }
 
         for(int i =0 ; i<m1.size() ; i++)
