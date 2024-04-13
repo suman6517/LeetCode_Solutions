34ea598a2cf80e83014cce6f2af834a0b5cfe081
Suman Baral <115060661+suman6517@users.noreply.github.com>
2024-04-13 06:07:03 +0530

    Time: 8 ms (64.43%), Space: 9.9 MB (6.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,27 +6,45 @@ public:
         {
             return false;
         }
-        map<char ,int>m1;
-        map<char,int>m2;
+        // unordered_map<char ,int>m1;
+        // unordered_map<char,int>m2;
 
-        for(auto it1 : s)
+        // for(auto it1 : s)
+        // {
+        //     m1[it1]++;
+
+        // }
+        // for(auto it2 : t)
+        // {
+        //     m2[it2]++;
+
+        // }
+
+        // for(int i =0 ; i<m1.size() ; i++)
+        // {
+        //     if (m1[i] != m2[i])
+        //     {
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        unordered_map<char , int> count;
+
+        for (auto it1: s)
         {
-            m1[it1]++;
-
+            count[it1] ++;
         }
-        for(auto it2 : t)
-        {
-            m2[it2]++;
 
-        }
-
-        for(int i =0 ; i<m1.size() ; i++)
+        for (auto it2 :t)
         {
-            if (m1[i] != m2[i])
+            if (count.find(it2) == count.end() || count[it2] == 0)
             {
                 return false;
             }
+            count[it2]--;
         }
+
         return true;
 
 
