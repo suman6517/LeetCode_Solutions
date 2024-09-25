7b4cc056f2e103794a666ee980aceccafb50f72a
Suman Baral <115060661+suman6517@users.noreply.github.com>
2024-09-25 21:21:20 +0530

    Time: 11 ms (36.05%), Space: 20.4 MB (53.52%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -17,11 +17,10 @@ bool valid(TreeNode* root , long min , long max)
     {
         return true;
     }
-    // if(root ->val < min && root->val >max)
-    // {
-    //     return true;
-    // }
-    if (!(root->val > min && root->val < max)) return false;
+    if(root ->val <=min || root->val >=max)
+    {
+        return false;
+    }
     return valid(root ->left , min , root ->val) && valid(root ->right , root ->val , max);
 }
     bool isValidBST(TreeNode* root) 
