0aa8abbe5bcbb4fb699768de58bac47f3eacfafe
Suman Baral <115060661+suman6517@users.noreply.github.com>
2024-10-07 22:56:06 +0530

    Time: 47 ms (24.85%), Space: 44.8 MB (50.29%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) 
     {
-         sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-        
-        int childIndex = 0;  // Index for children
-        int cookieIndex = 0; // Index for cookies
-        int m = g.size();    // Number of children
-        int n = s.size();    // Number of cookies
-        
-        // Try to satisfy each child greed factor with a cookie
-        while (childIndex < m && cookieIndex < n)
+        sort(g.begin() , g.end());
+        sort(s.begin() , s.end());
+        int childIndex = 0 , cookieIndex = 0;
+        int m = g.size() , n = s.size();
+        while(childIndex < m && cookieIndex < n)
         {
-            // If current cookie can satisfy the current child's greed
-            if (s[cookieIndex] >= g[childIndex]) 
+            if(g[childIndex] <= s[cookieIndex])
             {
-                childIndex++;  // Move to the next child
+               childIndex ++;
             }
-            // Move to the next cookie
             cookieIndex++;
         }
-        
-        // The number of children that are satisfied
         return childIndex;
     }
 };
