30d988d3bc62f1d26e657bdc159b410b78b05212
Suman Baral <115060661+suman6517@users.noreply.github.com>
2024-11-01 23:56:35 +0530

    Time: 28 ms (49.35%), Space: 42.8 MB (30.23%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    string makeFancyString(string s) {
-        if (s.length() <= 2) 
+    string makeFancyString(string s) 
+    {
+        if(s.length() <=2)
         {
             return s;
         }
-
-        string ans = "";
-
-        for (int i = 0; i < s.length() - 2; i++) {
-
-        if((s[i] == s[i+1]) && (s[i] == s[i+2]))
+        string ans ="";
+        for (int i=0; i<s.length()-2; i++)
         {
-            continue;
+            if(s[i]==s[i+1] && s[i]==s[i+2])
+            {
+                continue;
+            }
+            ans+=s[i];
         }
-        ans += s[i];
-        }
-
-        ans += s[s.length()-2];
-        ans += s[s.length()-1];
-
+        ans+=s[s.length()-2];
+        ans+=s[s.length()-1];
         return ans;
-
-
     }
 };
