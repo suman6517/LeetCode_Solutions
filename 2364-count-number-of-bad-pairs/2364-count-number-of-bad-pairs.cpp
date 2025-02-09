25b86c971c27a88689a8a63323e5586893fc1eb5
Suman Baral <115060661+suman6517@users.noreply.github.com>
2025-02-10 01:45:47 +0530

    Time: 69 ms (56.15%), Space: 87.6 MB (70.42%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -1,34 +1,19 @@
-// class Solution {
-// public:
-//     long long countBadPairs(vector<int>& nums) 
-//     {
-//         unordered_map<int , int>freq;
-//         int length = nums.size();
-//         long long goodPairs = 0;
-//         for(int i =0; i<length; i++)
-//         {
-//             int key = nums[i] -i;
-//             goodPairs += freq[key];
-//             freq[key]++;
-//         }
-//         // long long totalPairs = (length*(length-1))/2;
-//         // long long badPairs = totalPairs - goodPairs;
-//         // return badPairs;
-//         return (length*(length-1))/2 -goodPairs;
-//     }
-// };
 class Solution {
 public:
-    long long countBadPairs(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        long long goodPairs = 0, n = nums.size();
-        
-        for (int i = 0; i < n; i++) {
-            int key = nums[i] - i;
+    long long countBadPairs(vector<int>& nums) 
+    {
+        unordered_map<int , int>freq;
+        long long length = nums.size();
+        long long goodPairs = 0;
+        for(int i =0; i<length; i++)
+        {
+            int key = nums[i] -i;
             goodPairs += freq[key];
             freq[key]++;
         }
-        
-        return (n * (n - 1)) / 2 - goodPairs;
+        // long long totalPairs = (length*(length-1))/2;
+        // long long badPairs = totalPairs - goodPairs;
+        // return badPairs;
+        return (length*(length-1))/2 -goodPairs;
     }
 };
