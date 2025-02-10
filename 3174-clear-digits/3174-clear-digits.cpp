d7b0881e6f5f78183205caac67835287a9189ff1
Suman Baral <115060661+suman6517@users.noreply.github.com>
2025-02-11 01:08:01 +0530

    Time: 0 ms (100%), Space: 8.4 MB (70.3%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -3,18 +3,19 @@ public:
     string clearDigits(string s) 
     {
         string ans;
-        for(char i : s)
+        for(int i=0; i<s.length() ; i++)
         {
-            if(i>= '0' && i <='9')
+            if(s[i]>= '0' && s[i] <='9')
             {
                 if(!ans.empty())
                 {
                     ans.pop_back();
                 }
+                
             }
             else
                 {
-                    ans.push_back(i);
+                    ans.push_back(s[i]);
                 }
         }
         return ans;
