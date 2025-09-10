7402597de63e727e157ae9f8ca41fbc4ba253b41
Suman Baral <115060661+suman6517@users.noreply.github.com>
2025-09-10 17:49:14 +0530

    Time: 3 ms (31.65%), Space: 15.3 MB (21.8%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
- void getSubsets(vector<vector<int>>& ans, int ind, vector<int>& nums, vector<int> output)
- {
-    if (ind >= nums.size())
-        {
-            ans.push_back(output);
-            return ;
-        }
-    //elclude
-    getSubsets(ans , ind+1 , nums , output);
+void printSubsets(int currentIndex ,vector<vector<int>>&ans,vector<int>&temp , int size , vector<int>nums)
+{
+    // Base Condition
+    if(currentIndex >=size)
+    {
+        ans.push_back(temp);
+        return;
+    }
 
-    //include
-    int element =  nums[ind];
-    output.push_back(element);
-    getSubsets(ans , ind+1 , nums , output);
+    // include The number
+    temp.push_back(nums[currentIndex]);
+    printSubsets (currentIndex+1, ans, temp , size, nums);
 
+    // Exclude the number
+    temp.pop_back();
+    printSubsets(currentIndex+1, ans, temp , size, nums);
+}
 
- }
     vector<vector<int>> subsets(vector<int>& nums) 
     {
         vector<vector<int>>ans;
-        int ind = 0;
-        vector<int>output;
+        vector<int>temp;
+        int size = nums.size();
+        printSubsets(0 , ans,temp,size,nums);
 
-        getSubsets(ans, ind, nums, output);
-        return ans ;
+        return ans;
     }
 };
