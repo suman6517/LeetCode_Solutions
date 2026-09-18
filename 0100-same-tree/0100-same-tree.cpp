a0c29b87f5bdb74381762eaddb73f8d1778af686
Suman Baral <115060661+suman6517@users.noreply.github.com>
2026-09-19 03:10:58 +0530

    0 ms (100%) 12.7 MB (96.69%) 09-19-2026 0100-same-tree Easy .cpp Tree

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -13,19 +13,20 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) 
     {
-       if (p == nullptr && q == nullptr) 
-       {
+        if(p == NULL && q == NULL)
+        {
             return true;
-       }
-        
-        if (p == nullptr || q == nullptr) 
+        }
+        if(p == NULL || q == NULL)
         {
             return false;
         }
-          return (p->val == q->val) &&
-               isSameTree(p->left, q->left) &&
-               isSameTree(p->right, q->right);
+        if(p ->val != q->val)
+        {
+            return false;
+        }
+        return isSameTree(p -> left , q -> left) &&
+               isSameTree(p -> right , q-> right);
 
-        
     }
 };
