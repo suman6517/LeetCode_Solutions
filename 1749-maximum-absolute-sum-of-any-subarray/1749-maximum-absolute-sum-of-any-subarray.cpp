62683a554266c91d274992543529c0e4cf8e9e27
Suman Baral <115060661+suman6517@users.noreply.github.com>
2026-09-26 11:26:18 +0530

    0 ms (100%) 45.3 MB (26.72%) 09-26-2026 1749-maximum-absolute-sum-of-any-subarray Medium .cpp Array

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,21 +1,45 @@
-        class Solution {
+class Solution {
 public:
-    int maxAbsoluteSum(std::vector<int>& nums) {
-        int n = nums.size();
-        int maxEndingHere = nums[0];
-        int minEndingHere = nums[0];
-        int maxSoFar = nums[0];
-        int minSoFar = nums[0];
-        
-        // Kadane's algorithm to find max and min subarray sums
-        for (int i = 1; i < n; i++) {
-            maxEndingHere = std::max(nums[i], maxEndingHere + nums[i]);
-            minEndingHere = std::min(nums[i], minEndingHere + nums[i]);
-            maxSoFar = std::max(maxSoFar, maxEndingHere);
-            minSoFar = std::min(minSoFar, minEndingHere);
+int maxSum(vector<int>& nums)
+{
+    int ans = nums[0];
+        int maxSum = nums[0];
+
+        for(int i =1; i<nums.size(); i++)
+        {
+            int v1 = nums[i];
+            int v2 =maxSum+v1;
+
+            maxSum = max(v1,v2);
+
+            ans = max(ans,maxSum);
         }
-        
-        // The maximum absolute sum is the maximum of the absolute values of maxSoFar and minSoFar
-        return std::max(std::abs(maxSoFar), std::abs(minSoFar));
+    return abs(ans);
+}
+
+int minSum(vector<int>& nums)
+{
+        int ans = nums[0];
+        int minSum = nums[0];
+
+        for(int i =1; i<nums.size(); i++)
+        {
+            int v1 = nums[i];
+            int v2 =minSum+v1;
+
+            minSum = min(v1,v2);
+
+            ans = min(ans,minSum);
+        }
+        return abs(ans);
+
+}
+    
+    int maxAbsoluteSum(vector<int>& nums) 
+    {
+        int minResult = minSum(nums);
+        int maxResult = maxSum(nums);
+        return max(minResult ,maxResult);
+
     }
 };
